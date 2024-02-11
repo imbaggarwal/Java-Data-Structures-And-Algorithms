995a036b675b8a055262422b9c388d7509906336
Bhavya Aggarwal <51106185+imbaggarwal@users.noreply.github.com>
2024-02-11 14:05:14 +0530

    commit pattern 22

---
+++ src/Patterns/Pattern22.java
@@ -0,0 +1,35 @@
+package Patterns;
+
+import java.util.Scanner;
+
+/*
+        4 4 4 4 4 4 4
+        4 3 3 3 3 3 4
+        4 3 2 2 2 3 4
+        4 3 2 1 2 3 4
+        4 3 2 2 2 3 4
+        4 3 3 3 3 3 4
+        4 4 4 4 4 4 4
+ */
+public class Pattern22 {
+    static void printPattern(int n) {
+        for(int i=0; i<2*n-1; i++){
+            for(int j=0; j<2*n-1; j++){
+                int top = i;
+                int left = j;
+                int right = 2 * (n - 1) - j;
+                int bottom = 2 * (n - 1) - i;
+
+                System.out.print(n - Math.min(Math.min(top, bottom), Math.min(left, right)));
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        printPattern(n);
+    }
+}
